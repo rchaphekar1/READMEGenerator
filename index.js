c79f6462526c84fef98fb16ecae16294ee802660
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-01-24 01:31:06 -0600

    Added initial list of questions

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const fs = require('fs');
 
 // array of questions for user
 const questions = [
-
+    "What is the name of your project?",
+    "Please provide a link to your Github repo.",
+    "Please provide a quick description of your project.",
+    "Please provide a few key features of your project.",
+    "What is your name?",
+    "Please provide a link to your personal Github."
 ];
 
 // function to write README file
