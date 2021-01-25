c98c9d68d4eb955e6b0c52a7abb3cb102aca3888
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-01-24 18:04:22 -0600

    Added inquirer code to append to README file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# READMEGenerator
+# READMEGenerator{
+	"projectName": "README Generator",
+	"githubRepo": "yes",
+	"description": "yes",
+	"keyFeatures": "yes",
+	"userName": "Rahul",
+	"githubLink": "yes"
+}
--- index.js
+++ index.js
@@ -13,12 +13,52 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'projectName',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'githubRepo',
+                message: questions[1],
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: questions[2],
+            },
+            {
+                type: 'input',
+                name: 'keyFeatures',
+                message: questions[3],
+            },
+            {
+                type: 'input',
+                name: 'userName',
+                message: questions[4],
+            },
+            {
+                type: 'input',
+                name: 'githubLink',
+                message: questions[5],
+            },
+        ])
+        .then((data) => {
+            const fileName = `${"README"}.md`;
+
+            fs.appendFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+                err ? console.log(err) : console.log("Success!")
+            );
+        });
+};
 
 // function to initialize program
 function init() {
-
-}
+    writeToFile();
+};
 
 // function call to initialize program
 init();
