2dbd3aa9abfc1f3882fe9570ac2536d101f8514e
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-01-25 13:26:55 -0600

    Updated init function to more closely resemble in class example

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-# READMEGenerator{
-	"projectName": "README Generator",
-	"githubRepo": "yes",
-	"description": "yes",
-	"keyFeatures": "yes",
-	"userName": "Rahul",
-	"githubLink": "yes"
-}
+# README Generator
+
+<hr>
+
+## Description
+
+https://github.com/rchaphekar1/READMEGenerator
+
+An application that generates a README.md file using node.js.
+
+## Credits
+
+Rahul Chaphekar: https://github.com/rchaphekar1
+
+## License
+
+Copyright © 2021 Rahul Chaphekar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
     "What is the name of your project?",
     "Please provide a link to your Github repo.",
     "Please provide a quick description of your project.",
-    "Please provide a few key features of your project.",
     "What is your name?",
     "Please provide a link to your personal Github."
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+const writeToFile = () =>
     inquirer
         .prompt([
             {
@@ -32,32 +34,62 @@ function writeToFile(fileName, data) {
             },
             {
                 type: 'input',
-                name: 'keyFeatures',
+                name: 'userName',
                 message: questions[3],
             },
             {
                 type: 'input',
-                name: 'userName',
+                name: 'githubLink',
                 message: questions[4],
             },
-            {
-                type: 'input',
-                name: 'githubLink',
-                message: questions[5],
-            },
-        ])
-        .then((data) => {
-            const fileName = `${"README"}.md`;
+        ]);
 
-            fs.appendFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-                err ? console.log(err) : console.log("Success!")
-            );
-        });
-};
+const generateREADME = (data) =>
+`# ${data.projectName}
+
+// Include screenshot or gif of application here
+
+<hr>
+
+## Description
+
+${data.githubRepo}
+
+${data.description}
+
+## Credits
+
+${data.userName}: ${data.githubLink}
+
+## License
+
+Copyright © 2021 ${data.userName}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+`;
 
 // function to initialize program
 function init() {
-    writeToFile();
+    writeToFile()
+        .then((data) => writeFileAsync('README.md', generateREADME(data)))
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.log(err));
 };
 
 // function call to initialize program
